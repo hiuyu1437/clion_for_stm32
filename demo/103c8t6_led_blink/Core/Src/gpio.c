acafb778edbb4d5a08db6358d33e7475b2cb0502
Bill Jack <111075017+BFmHNO3@users.noreply.github.com>
2025-04-30 22:32:06 +0800

    feat(demo): 配置 LED 的 GPIO
    在 STM32CubeMX 中配置 5 个 LED 的 GPIO，并设置相关的标签。

--- demo/103c8t6_led_blink/Core/Src/gpio.c
+++ demo/103c8t6_led_blink/Core/Src/gpio.c
@@ -42,10 +42,25 @@
 void MX_GPIO_Init(void)
 {
 
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_WHITE_Pin|LED_YELLOW_Pin|LED_BLUE_Pin|LED_RED_Pin
+                          |LED_GREEN_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : LED_WHITE_Pin LED_YELLOW_Pin LED_BLUE_Pin LED_RED_Pin
+                           LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_WHITE_Pin|LED_YELLOW_Pin|LED_BLUE_Pin|LED_RED_Pin
+                          |LED_GREEN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
